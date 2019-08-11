d77263bcdceab275e1e25445c27132586e95ce13
Kat <vampirebatkat@gmail.com>
2019-08-11 20:13:03 +0100

    fixed redirect bug

--- app.rb
+++ app.rb
@@ -1,23 +1,33 @@
 require 'sinatra/base'
 require 'sinatra'
 
-class Battle < Sinatra::Base
-  enable :sessions
+class Rocky < Sinatra::Base
+    enable :sessions
+  
+    get '/' do
+      erb :index
+    end
+  
+    post '/name' do
+      session[:player_name] = params[:player_name]
+      redirect '/name'
+    end
+  
+    get '/name' do
+      @player_name = session[:player_name]
+      erb :name
+    end
 
-  get '/' do
-    erb :index
+    post '/move' do
+      session[:player_move] = params[:player_move]
+      redirect '/play'
+    end
+
+    get '/play' do
+      @player_move = session[:player_move]
+      erb :play
+    end
+  
+    # start the server if ruby file executed directly
+    run! if app_file == $0
   end
-
-  post '/name' do
-    session[:player_name] = params[:player_name]
-    redirect '/play'
-  end
-
-  get '/play' do
-    @player_name = session[:player_name]
-    erb :play
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
