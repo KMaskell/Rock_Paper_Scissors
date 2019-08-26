8f3bfc3d3435617c26deac9dbbab3b16b3893c3d
Kat <vampirebatkat@gmail.com>
2019-08-26 16:22:11 +0100

    index, move, result working

---
+++ README.txt
@@ -0,0 +1,9 @@
+to clear up opened ports type:
+
+ps aux | grep ruby
+
+This will show you a list of opened ports. To close type:
+
+kill -9 29594
+
+...but use the correct 5-digit number of port you want to close.
--- app.rb
+++ app.rb
@@ -12,13 +12,23 @@ class Rocky < Sinatra::Base
       session[:player_name] = params[:player_name]
       redirect '/move'
     end
-  
+
     get '/move' do
       @player_name = session[:player_name]
       erb :move
     end
 
-    post '/move' do
+    post '/rock' do
+      session[:player_move] = params[:player_move]
+      redirect '/result'
+    end
+
+    post '/paper' do
+      session[:player_move] = params[:player_move]
+      redirect '/result'
+    end
+
+    post '/scissors' do
       session[:player_move] = params[:player_move]
       redirect '/result'
     end
@@ -27,11 +37,8 @@ class Rocky < Sinatra::Base
       @player_move = session[:player_move]
       erb :result
     end
-
-    post '/result' do
-      erb :result
-    end
   
     # start the server if ruby file executed directly
     run! if app_file == $0
+
   end
